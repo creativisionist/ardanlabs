ca3a6f7b9df48b6ee6b8484c5d5c9169f438206d
creativisionist <creativisionist@gmail.com>
2016-09-14 14:12:05 -0500

    Add edits so webserver won't respond to just any given route.

--- webserver.go
+++ webserver.go
@@ -5,13 +5,23 @@ import (
   "html"
   "log"
   "net/http"
+
+  "github.com/gorilla/mux"
 )
 
 func main() {
-  http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "Hello World from %q", html.EscapeString(r.URL.Path))
-  })
+  // Create a router
+  rt := mux.NewRouter().StrictSlash(true)
 
+  // Add the "index" or root path
+  rt.HandleFunc("/", Index)
+
+  // Fire up the server
   log.Println("Starting server on http://localhost:8080")
-  log.Fatal(http.ListenAndServe(":8080", nil))
+  log.Fatal(http.ListenAndServe(":8080", rt))
+}
+
+// Index is the "index" handler
+func Index(w http.ResponseWriter, r *http.Request) {
+  fmt.Fprintf(w, "Hello World from %q", html.EscapeString(r.URL.Path))
 }
